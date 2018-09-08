de48a6f239649ad72bcd1cf2f4f76a0cbb038b8d
phixyn <phixyn@gmail.com>
2018-09-08 16:13:36 +0100

    WIP: add onClick handler for addTaskButton

--- app/src/main/java/com/phixyn/notephix/MainActivity.java
+++ app/src/main/java/com/phixyn/notephix/MainActivity.java
@@ -1,31 +1,49 @@
 package com.phixyn.notephix;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText taskTextField;
+    private Button addTaskButton;
+    private ListView taskList;
+
+    private ArrayList<String> tasksArray;
+    private ArrayAdapter<String> tasksAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        taskTextField = findViewById(R.id.item_edit_text);
+        addTaskButton = findViewById(R.id.add_btn);
+        taskList = findViewById(R.id.items_list);
+
+        addTaskButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +66,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.add_btn:
+                String taskEntered = taskTextField.getText().toString();
+                tasksAdapter.add(taskEntered);
+                taskTextField.setText("");
+
+                Toast.makeText(this, "Task added", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
